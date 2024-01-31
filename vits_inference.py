54e7c991facd7bf50853a477d60b1dd9aba61e93
ThaiLe1220 <lehongthai2000@gmail.com>
2024-01-31 12:04:43 +0700

    simple format

--- vits_inference.py
+++ vits_inference.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import math
@@ -13,10 +12,16 @@ import io
 
 import commons
 import utils
-from data_utils import TextAudioLoader, TextAudioCollate, TextAudioSpeakerLoader, TextAudioSpeakerCollate
+from data_utils import (
+    TextAudioLoader,
+    TextAudioCollate,
+    TextAudioSpeakerLoader,
+    TextAudioSpeakerCollate,
+)
 from models import SynthesizerTrn
 from text.symbols import symbols
 from text import text_to_sequence
+
 
 def get_text(text, hps):
     text_norm = text_to_sequence(text, hps.data.text_cleaners)
@@ -24,6 +29,7 @@ def get_text(text, hps):
         text_norm = commons.intersperse(text_norm, 0)
     text_norm = torch.LongTensor(text_norm)
     return text_norm
+
 
 def save_audio_as_mp3(audio, sample_rate, file_name):
     # Convert the numpy array to bytes
@@ -34,9 +40,10 @@ def save_audio_as_mp3(audio, sample_rate, file_name):
     # Convert the bytes to AudioSegment
     audio_bytes.seek(0)  # Go to the start of BytesIO object
     audio_segment = AudioSegment.from_wav(audio_bytes)
-    
+
     # Export as MP3
     audio_segment.export(file_name, format="mp3")
+
 
 # Single Speaker
 hps = utils.get_hparams_from_file("checkpoints/config.json")
@@ -45,7 +52,8 @@ net_g = SynthesizerTrn(
     len(symbols),
     hps.data.filter_length // 2 + 1,
     hps.train.segment_size // hps.data.hop_length,
-    **hps.model).cuda()
+    **hps.model
+).cuda()
 _ = net_g.eval()
 
 _ = utils.load_checkpoint("checkpoints/G_epoch_10.pth", net_g, None)
@@ -54,7 +62,14 @@ stn_tst = get_text("こんにちは", hps)
 with torch.no_grad():
     x_tst = stn_tst.cuda().unsqueeze(0)
     x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
-    audio = net_g.infer(x_tst, x_tst_lengths, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+    audio = (
+        net_g.infer(
+            x_tst, x_tst_lengths, noise_scale=0.667, noise_scale_w=0.8, length_scale=1
+        )[0][0, 0]
+        .data.cpu()
+        .float()
+        .numpy()
+    )
 
 # Save single speaker audio
 save_audio_as_mp3(audio, hps.data.sampling_rate, "single_speaker_synthesized_audio.mp3")
@@ -67,7 +82,8 @@ net_g = SynthesizerTrn(
     hps.data.filter_length // 2 + 1,
     hps.train.segment_size // hps.data.hop_length,
     n_speakers=hps.data.n_speakers,
-    **hps.model).cuda()
+    **hps.model
+).cuda()
 _ = net_g.eval()
 
 _ = utils.load_checkpoint("checkpoints/G_epoch_10.pth", net_g, None)
@@ -77,7 +93,21 @@ with torch.no_grad():
     x_tst = stn_tst.cuda().unsqueeze(0)
     x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).cuda()
     sid = torch.LongTensor([4]).cuda()
-    audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+    audio = (
+        net_g.infer(
+            x_tst,
+            x_tst_lengths,
+            sid=sid,
+            noise_scale=0.667,
+            noise_scale_w=0.8,
+            length_scale=1,
+        )[0][0, 0]
+        .data.cpu()
+        .float()
+        .numpy()
+    )
 
 # Save multiple speakers audio
-save_audio_as_mp3(audio, hps.data.sampling_rate, "multiple_speakers_synthesized_audio.mp3")
+save_audio_as_mp3(
+    audio, hps.data.sampling_rate, "multiple_speakers_synthesized_audio.mp3"
+)
