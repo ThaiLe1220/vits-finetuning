4936326367e59cc3b30442c65bf32ea706fd94a6
ThaiLe1220 <lehongthai2000@gmail.com>
2024-02-01 20:21:41 +0700

    increase checkpoint_save

--- train_ms.py
+++ train_ms.py
@@ -158,7 +158,7 @@ def run(rank, n_gpus, hps):
             checkpoint_path_d = os.path.join(hps.model_dir, f"D_epoch_{epoch}.pth")
 
             # Save checkpoint every 10 epochs
-            if epoch % 10 == 0:
+            if epoch % 200 == 0:
                 utils.save_checkpoint(
                     net_g, optim_g, hps.train.learning_rate, epoch, checkpoint_path_g
                 )
