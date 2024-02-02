0eecf6604c91bbf5acd59012149c94dccfb830aa
ThaiLe1220 <lehongthai2000@gmail.com>
2024-02-02 11:51:48 +0700

    update vits_inference

--- vits_inference.py
+++ vits_inference.py
@@ -89,7 +89,7 @@ net_g = SynthesizerTrn(
 _ = net_g.eval()
 
 _ = utils.load_checkpoint(
-    "../drive/MyDrive/vits-finetune/checkpoints/G_epoch_1000.pth", net_g, None
+    "../drive/MyDrive/vits-finetune/checkpoints/G_epoch_10000.pth", net_g, None
 )
 
 stn_tst = get_text(
