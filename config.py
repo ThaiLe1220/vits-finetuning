0becd33ed00013b73d46c147cf4aaaee7d2ef8b0
ThaiLe1220 <lehongthai2000@gmail.com>
2024-02-02 11:22:19 +0700

    add training data

--- config.py
+++ config.py
@@ -21,3 +21,44 @@ config["data"]["text_cleaners"] = TEXT_CLEANERS
 
 with open("configs/config.json", "w+") as f:
     json.dump(config, f, indent=4)
+
+
+# @title  Process the wav files of the dataset to make them meet the requirements (Optional)
+import os
+import librosa
+from tqdm import tqdm
+
+wav_path = "wav/ba/miyu"  # @param {type:"string"}
+for file_name in tqdm(os.listdir(wav_path)):
+    if file_name.endswith(".spec.pt"):
+        os.remove(rf"{wav_path}/{file_name}")
+        continue
+    y, sr = librosa.load(rf"{wav_path}/{file_name}", sr=22050, mono=True)
+    sf.write(rf"{wav_path}/{file_name}", y, 22050, subtype="PCM_16")
+
+
+# # Define the input and output file paths
+# input_file_path = "_eugene/obama2.txt"
+# output_file_path = "_eugene/obama2_cleaned.txt"
+
+# # Define the prefix to add to each filename
+# prefix = "wav/obama2/"
+
+# # Open the input file for reading and the output file for writing
+# with open(input_file_path, "r", encoding="utf-8") as input_file, open(
+#     output_file_path, "w", encoding="utf-8"
+# ) as output_file:
+#     # Iterate over each line in the input file
+#     for line in input_file:
+#         # Split the line into the filename part and the rest of the line
+#         parts = line.split("|", 1)
+#         if len(parts) == 2:
+#             # Prepend the prefix to the filename part
+#             modified_line = f"{prefix}{parts[0].strip()}|{parts[1]}"
+#             # Write the modified line to the output file
+#             output_file.write(modified_line)
+#         else:
+#             # If the line does not match the expected format, write it unchanged
+#             output_file.write(line)
+
+# print(f"Lines have been modified and written to {output_file_path}")
